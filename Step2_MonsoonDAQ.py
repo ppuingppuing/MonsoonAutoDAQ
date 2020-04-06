eb49e0c56c375d149b738b833a5c43381b945025
ppuing <ppuing@hotmail.com>
2020-04-06 13:43:48 +0900

    ADD:threads, adb subprocess

--- Step2_MonsoonDAQ.py
+++ Step2_MonsoonDAQ.py
@@ -3,14 +3,18 @@ import Monsoon.sampleEngine as sampleEngine
 import Monsoon.Operations as op
 
 from datetime import datetime
+from time import sleep
+
 import threading
+import subprocess
 
 
 count = 0
 
+
 class MonsoonDAQ:
 
-    print("Monsoon Set up...")
+    print("Monsoon Set up...")  # 이거 뭐지..왜 돌지
     mymon = LVPM.Monsoon()
     mymon.setup_usb()
     mymon.setVout(3.9)
@@ -41,10 +45,22 @@ class MonsoonDAQ:
         threading.Timer(3.88, self.getsamples).start()
         count += 1
 
+    def setimages(self):
+        subprocess.call("adb devices", shell=True)
+        print("[", datetime.utcnow().strftime('%H:%M:%S.%f'), "] Daemon started... Unplug the phone!!!")
+
 
 def main():
     mymonsoon = MonsoonDAQ()
-    mymonsoon.getsamples()
+    # mymonsoon.getsamples()
+
+    thread1 = threading.Thread(target=mymonsoon.getsamples(), args=())
+    thread2 = threading.Thread(target=mymonsoon.setimages(), args=())
+
+    sleep(10)
+
+    thread1.start()
+    thread2.start()
 
 
 if __name__ == "__main__":
